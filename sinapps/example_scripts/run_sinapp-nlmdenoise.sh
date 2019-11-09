73b1201f6705fdbe5d7d19e0beaa9bade7e2251f
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2019-11-09 14:33:59 +0100

    On going challenge

--- sinapps/example_scripts/run_sinapp-nlmdenoise.sh
+++ sinapps/example_scripts/run_sinapp-nlmdenoise.sh
@@ -2,8 +2,12 @@
 
 bids_dir=/Users/sebastientourbier/Softwares/mialsuperresolutiontoolkit/data
 
-docker run --rm -u $(id -u):$(id -g) \
+rm -R "${bids_dir}/derivatives/bids_demo"
+
+docker run --rm \
+	-u $(id -u):$(id -g) \
 	-v /var/run/docker.sock:/var/run/docker.sock \
+	-v /usr/local/bin/docker:/usr/bin/docker \
 	-v ${bids_dir}:/fetaldata \
 	supermri-team/sinapp-nlmdenoise:0.1 \
 	/fetaldata /fetaldata/derivatives participant --participant_label 01
